6530c9e31a642ad783c23cfe584570823fd7c381
Ambros94 <luca_ambro94@hotmail.it>
2016-07-25 13:42:06 +0200

    Improved GUI feedback. Added command receiver as websocket server

--- renderer.js
+++ renderer.js
@@ -5,19 +5,41 @@ var jQuery = require("jQuery");
 var io = require('socket.io-client');
 
 var serverUrl = 'http://localhost:8887';
-var conn = io.connect(serverUrl);
+var socketClient = io.connect(serverUrl);
 
 /**
  * When send button is pressed send the input text value to the Brain
  */
 jQuery("#send").on("click", function () {
-    let message = jQuery("#textCommand").val();
-    console.log(message);
-    conn.emit('textCommand', message, function (resp, data) {
-        console.log('server sent resp code ' + resp);
-        if (resp === '200') {// Command succesfully received from server
+    // Remove previously displayed thumb
+    jQuery("#thumbUp").css("display", "none");
+    jQuery("#thumbDown").css("display", "none");
+    socketClient.emit('textCommand', jQuery("#textCommand").val(), function (resp, data) {
+        console.log('Server sent resp code ' + resp);
+        if (resp === 200) {// Command succesfully received from server
+            console.log("good");
             jQuery("#thumbUp").css("display", "inline-block");
-        } else if (resp === '404')
-            jQuery("#thumbDown").css("display", "inline-block");
+        } else if (resp === 404) {
+            console.log("bad");
+        }
+    });
+});
+
+/**
+ * Socket.io server
+ */
+var fs = require('fs')
+    , http = require('http')
+    , socketServer = require('socket.io');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end("Server index page!");
+}).listen(5678, function () {
+    console.log('Listening at: http://localhost:5678');
+});
+
+socketServer.listen(server).on('connection', function (socket) {
+    socket.on('command', function (msg) {
+        jQuery("#jsonCommand").val(msg);
     });
 });
